2f9f88482d1c4ed8623f6f3badd6f6888ed01e81
mypark0727 <pugas0727@gmail.com>
2025-01-14 13:38:59 +0900

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,12 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* basename을 선언하면 그 하위요소에서 path를 설정할때 생략가능 */}
-    <BrowserRouter basename="/myReact">
+    {/* 
+      basename을 선언하면 그 하위요소에서 path를 설정할때 생략가능 
+      config.js 에서 base 추가하여 사용
+    */}
+    {/* <BrowserRouter basename="/myReact"> */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/myReact/', 
+  // base: '/myReact/', 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // '@'를 'src'로 설정
